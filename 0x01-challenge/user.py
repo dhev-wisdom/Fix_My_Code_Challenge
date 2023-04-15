6444a14abc0b44922d89c955a963362956dad233
dhev-wisdom <nonsowisdom62@gmail.com>
2023-04-15 04:45:34 +0100

    Fixed User class error

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -3,13 +3,18 @@
 User class
 """
 
-class User():
+class User:
     """ Documentation """
 
     def __init__(self):
         """ Documentation """
         self.__email = None
 
+    @property
+    def email(self):
+        """ Documentation """
+        return self.__email
+   
     @email.setter
     def email(self, value):
         """ Documentation """
@@ -17,11 +22,6 @@ class User():
             raise TypeError("email must be a string")
         self.__email = value
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
-   
     
 if __name__ == "__main__":
 
